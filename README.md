396294d007c8b3f61491878ed3d322e0bc109e28
cockiemnstr <164089329+cockiemnstr@users.noreply.github.com>
2024-03-21 01:20:54 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# data-harvest-navigator
+
+Navigate the data landscape with Harvest Navigator, a versatile tool for harvesting and processing blockchain data.
+
+## Installation
+
+```bash
+git clone https://github.com/cockiemnstr/data-harvest-navigator.git
+cd data-harvest-navigator
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+const data_harvest_navigator = require('data-harvest-navigator');
+const harvest_navi_prov = require('harvest-navi-prov');
+const pg = require('pg');
+const react_dom = require('react-dom');
+const web3_utils = require('web3-utils');
+const ethereumjs_util = require('ethereumjs-util');
+const babel_core = require('babel-core');
+const react_redux = require('react-redux');
+const axios = require('axios');
+const mysql = require('mysql');
+
+const util = require('util');
+const formatted = util.format('This %s a %s', 'is', 'string');
+console.log(formatted);
+
+// Create a JWT token using jsonwebtoken library
+const jwt = require('jsonwebtoken');
+const generateJWTToken = (payload, secret, options) => {
+  return jwt.sign(payload, secret, options);
+}
+const userPayload = { userId: 12345, username: 'john_doe' };
+const jwtSecret = 'secret_key';
+const jwtOptions = { expiresIn: '1h' };
+const token = generateJWTToken(userPayload, jwtSecret, jwtOptions);
+console.log('JWT Token:', token);
+
+const path = require('path');
+console.log(`Current working directory: ${path.resolve()}`);
+
+// Calculate the factorial of a number recursively
+const factorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log('Factorial of 5:', factorial(5));
+
+const removeDuplicates = (arr) => {
+  return [...new Set(arr)];
+};
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
+
+const zlib = require('zlib');
+const input = 'Compress me!';
+zlib.gzip(input, (err, buffer) => {
+  if (!err) {
+    console.log('Compressed data:', buffer.toString('base64'));
+  }
+});
+
+// Get the number of uncles for a block in Ethereum by block hash
+web3.eth.getBlockUncleCount('0xblockHash').then(uncles => {
+  console.log('Number of uncles by block hash:', uncles);
+}).catch(err => {
+  console.error('Error getting uncles by block hash:', err);
+});
